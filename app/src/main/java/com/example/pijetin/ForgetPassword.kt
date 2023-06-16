d0fddb50384126bcbc979eca4b7a170f0eacd9cb
sayfiqireal <ahmadmuflihtaufiqi@gmail.com>
2023-06-16 09:44:46 +0700

    addet forget password

---
+++ app/src/main/java/com/example/pijetin/ForgetPassword.kt
@@ -0,0 +1,11 @@
+package com.example.pijetin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForgetPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forget_password)
+    }
+}
