30f906ace285f7603436af9a4dd193ee22204aed
Kevin <kevinraihanr45@gmail.com>
2023-07-03 11:35:01 +0700

    Added POST Method for Login

--- app/src/main/java/com/example/pijetin/data/Api/UsersAPI.kt
+++ app/src/main/java/com/example/pijetin/data/Api/UsersAPI.kt
@@ -4,19 +4,24 @@ import com.example.pijetin.data.Network.NetworkClient
 import com.example.pijetin.data.Network.ResponseStatus
 import com.example.pijetin.data.Network.deserializeJson
 import com.example.pijetin.data.Network.mapFailedResponse
+import com.example.pijetin.data.Network.serialized
 import com.example.pijetin.data.model.User
 import com.example.pijetin.data.model.UserList
+import com.example.pijetin.data.model.UserLogin
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import okhttp3.Call
 import okhttp3.Callback
+import okhttp3.FormBody
+import okhttp3.Request
 import okhttp3.Response
 import org.json.JSONArray
 import java.io.IOException
 
 class UsersAPI {
     private val usersEndpoint = "/users"
+    private val loginEndpont = "/login"
     fun getUser(onResponse: (ResponseStatus<List<User>>) -> Unit) {
         val request = NetworkClient.requestBuilder(usersEndpoint)
         NetworkClient
@@ -54,6 +59,51 @@ class UsersAPI {
             })
     }
 
+    fun userLogin(email: String, password: String, onResponse: (ResponseStatus<User?>) -> Unit) {
+        val requestBody = FormBody.Builder()
+            .add("email", email)
+            .add("password", password)
+            .build()
+        val request = Request.Builder()
+            .url("https://pijat-in-be.onrender.com${loginEndpont}")
+            .post(requestBody)
+            .build()
+
+        NetworkClient
+            .client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val responseData = response.body?.string() ?: ""
+                    val moshi = Moshi.Builder().build()
+                    val adapter: JsonAdapter<User> = moshi.adapter(User::class.java)
+                    val user = adapter.fromJson(responseData)
+                    onResponse.invoke(
+                        ResponseStatus.Success(
+                            data = user,
+                            method = "POST",
+                            status = true
+                        )
+                    )
+                } else {
+                    onResponse.invoke(
+                        mapFailedResponse(response)
+                    )
+                }
+                response.body?.close()
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                onResponse.invoke(
+                    ResponseStatus.Failed(
+                        code = -1,
+                        message = e.message.toString(),
+                        throwable = e
+                    )
+                )
+            }
+        })
+    }
+
     fun getError(onResponse: (ResponseStatus<Nothing>) -> Unit) {
         NetworkClient
             .client
@@ -69,4 +119,5 @@ class UsersAPI {
                 }
             })
     }
+
 }
---
+++ app/src/main/java/com/example/pijetin/data/model/UserLogin.kt
@@ -0,0 +1,6 @@
+package com.example.pijetin.data.model
+
+data class UserLogin(
+    val email: String,
+    val password: String
+)
