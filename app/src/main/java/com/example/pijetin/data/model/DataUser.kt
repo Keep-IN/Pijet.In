325c1ee6ce3f425596a7c75e401728bb12c11bcc
Kevin <kevinraihanr45@gmail.com>
2023-07-04 11:11:59 +0700

    Added onLoading and Finished Loading

---
+++ app/src/main/java/com/example/pijetin/data/model/DataUser.kt
@@ -0,0 +1,7 @@
+package com.example.pijetin.data.model
+
+object DataUser {
+    var nama: String = ""
+    var email: String = ""
+    var imageUrl: String = ""
+}
--- app/src/main/java/com/example/pijetin/feature/Login/Login.kt
+++ app/src/main/java/com/example/pijetin/feature/Login/Login.kt
@@ -3,9 +3,14 @@ package com.example.pijetin.feature.Login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
 import com.example.pijetin.data.Api.UsersAPI
+import com.example.pijetin.data.Network.ResponseStatus
+import com.example.pijetin.data.model.DataUser
+import com.example.pijetin.data.model.UserLoginResponse
 import com.example.pijetin.databinding.ActivityLoginBinding
 import com.example.pijetin.feature.Dashboard.NavBarContainer
 import com.example.pijetin.feature.ForgetPass.ForgetPassword
@@ -45,6 +50,7 @@ class Login : AppCompatActivity(), LoginContract {
 
         binding.btnMasukLogin.setOnClickListener{
             // cek eror button masuk
+            onLoading()
             presenter.loginUser(binding.tilEmailLogin.editText?.text.toString(),
                 binding.tilPwLogin.editText?.text.toString())
         }
@@ -55,11 +61,13 @@ class Login : AppCompatActivity(), LoginContract {
     }
 
     override fun onLoading() {
-        TODO("Not yet implemented")
+        binding.bgLoading.visibility = View.VISIBLE
+        binding.pbLoading.visibility = View.VISIBLE
     }
 
     override fun onFinishedLoading() {
-        TODO("Not yet implemented")
+        binding.bgLoading.visibility = View.GONE
+        binding.pbLoading.visibility = View.GONE
     }
 
     override fun onError(code: Int, message: String) {
@@ -78,14 +86,20 @@ class Login : AppCompatActivity(), LoginContract {
         TODO("Not yet implemented")
     }
 
-    override fun onSuccesLogin() {
+    override fun onSuccesLogin(user: UserLoginResponse?) {
         //fungsi pindah login ke dashboard + allert
+        if (user != null) {
+            DataUser.nama = user.nama
+            DataUser.email = user.email
+        }
         startActivity(Intent(this, NavBarContainer::class.java))
         Toast.makeText(this, "Login berhasil", Toast.LENGTH_SHORT).show()
+        onFinishedLoading()
     }
 
     override fun onErrorLogin(code: Int, message: String) {
         Toast.makeText(this, "Login gagal", Toast.LENGTH_SHORT).show()
+        onFinishedLoading()
     }
 
     override fun onErrorFalse(code: Int, message: String) {
